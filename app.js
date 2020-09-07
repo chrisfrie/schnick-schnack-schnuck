6f6c2579b625f36b787cbbe62ec316276fc4f6f6
Chris <christina.friede5@gmail.com>
2020-09-07 21:33:49 +0200

    Add a startGame method

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ new Vue({
         playerPoints: 0,
         brunoPoints: 0,
         gameIsRunning: false,
+    },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerPoints = 0;
+            this.brunoPoints = 0;
+        }
     }
 });
