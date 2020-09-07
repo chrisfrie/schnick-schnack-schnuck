5a1cea53e32947f0bd59675035901ebf57a4acd7
Chris <christina.friede5@gmail.com>
2020-09-07 23:00:24 +0200

    Add calculateWinnerPoints

--- app.js
+++ app.js
@@ -10,6 +10,36 @@ new Vue({
             this.gameIsRunning = true;
             this.playerPoints = 0;
             this.brunoPoints = 0;
+        },
+        calculateWinnerPoints: function() {
+            if(playerChoice == 'rock' && brunoChoice == 'rock'){
+                this.playerPoints += 0;
+                this.brunoPoints += 0;
+            } else if(playerChoice == 'rock' && brunoChoice == 'scissor'){
+                this.playerPoints += 1;
+                this.brunoPoints += 0;
+            } else if(playerChoice == 'rock' && brunoChoice == 'paper'){
+                this.playerPoints += 0;
+                this.brunoPoints += 1;
+            }  else if(playerChoice == 'scissor' && brunoChoice == 'rock'){
+                this.playerPoints += 0;
+                this.brunoPoints += 1;
+            } else if(playerChoice == 'scissor' && brunoChoice == 'scissor'){
+                this.playerPoints += 0;
+                this.brunoPoints += 0;
+            } else if(playerChoice == 'scissor' && brunoChoice == 'paper'){
+                this.playerPoints += 1;
+                this.brunoPoints += 0;
+            } else if(playerChoice == 'paper' && brunoChoice == 'rock'){
+                this.playerPoints += 1;
+                this.brunoPoints += 0;
+            } else if(playerChoice == 'paper' && brunoChoice == 'scissor'){
+                this.playerPoints += 0;
+                this.brunoPoints += 1;
+            } else {
+                this.playerPoints += 0;
+                this.brunoPoints += 0;
+            }
         }
     }
 });
